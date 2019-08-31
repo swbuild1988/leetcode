d1f0236fe6db6f7e41f228beb50b7da463dbe55c
shan wei <swbuild@qq.com>
2019-08-31 23:05:36 +0800

    26,27

--- 0027. 移除元素/main.py
+++ 0027. 移除元素/main.py
@@ -1,18 +1,19 @@
 class Solution(object):
-    def removeDuplicates(self, nums):
+    def removeElement(self, nums, val):
         """
         :type nums: List[int]
+        :type val: int
         :rtype: int
         """
         
         index = 0
-        while index < len(nums)-1:
-            if nums[index] == nums[index+1]:
-                nums.pop(index+1)
+        while index < len(nums):
+            if nums[index] == val:
+                nums.pop(index)
             else:
                 index += 1
         return len(nums)
 
 
 s = Solution()
-print(s.removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
+print(s.removeElement([0,1,2,2,3,0,4,2], 2))
