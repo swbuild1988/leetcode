13b3b61cb467674def32f2a3a48835726d135c23
shan wei <swbuild@qq.com>
2019-09-09 02:48:46 +0800

    完成32的栈解法，动规待解

--- 0032. 最长有效括号/main - 栈.py
+++ 0032. 最长有效括号/main - 栈.py
@@ -5,5 +5,22 @@ class Solution(object):
         :rtype: int
         """
 
+        stack = [-1]
+        maxLength = 0
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack.append(i)
+            else:
+                # 不管如何删掉最后一个，
+                # 如果是“（”，则完成一个配对
+                # 如果是“）”，说明前面都是没用的了，从当前开始计算有效
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    maxLength = max(maxLength, i-stack[-1])
+        return maxLength
+
+
 s = Solution()
-print(s.longestValidParentheses(")()()))((((()))))"))
+print(s.longestValidParentheses("()()"))
