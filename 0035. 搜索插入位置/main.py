4f86a7bbaca14e2b29b6e2165549cc4d7d84e9cf
shan wei <swbuild@qq.com>
2019-09-16 22:30:04 +0800

    35

---
+++ 0035. 搜索插入位置/main.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        left, right = 0, len(nums)-1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+        
+        return left
+
+s = Solution()
+print(s.searchInsert([5,7,8,9,10], 6))
