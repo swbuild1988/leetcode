93b8e48ef482eded74c3b6baff69cc3226f14e67
shan wei <swbuild@qq.com>
2019-10-17 00:41:11 +0800

    51.利用形参，就不需要标记再解除标记了

--- 0051. N皇后/main.py
+++ 0051. N皇后/main.py
@@ -116,6 +116,8 @@ class Solution2(object):
 # 把第 i 位置 0：a &= ~(1 << i)
 # 把第 i 位取反：a ^= (1 << i)
 # 读取第 i 位的值：(a >> i) & 1
+
+
 class Solution3(object):
     def solveNQueens(self, n):
         """
@@ -152,9 +154,9 @@ class Solution3(object):
                     continue
 
                 col[row] = i
-                self.shu |= (1<<i)
-                self.pie |= (1<<j)
-                self.na |= (1<<k)
+                self.shu |= (1 << i)
+                self.pie |= (1 << j)
+                self.na |= (1 << k)
                 solve(row+1)
                 col[row] = -1
                 self.shu &= ~(1 << i)
@@ -174,7 +176,7 @@ class Solution3(object):
 # ~a = 11001011
 # -a = 11001100
 # a & -a = 00000100
-#「-a」其实是个算术运算，上文中说过，它等于把 a 取反再加 1。
+# 「-a」其实是个算术运算，上文中说过，它等于把 a 取反再加 1。
 
 #   当试探到第 row 行时，如果能用一个 bit array 表示出这一行上能放置皇后的位置，就可以用上述循环直接枚举它们，跳过那些已经不能放置皇后的位置了。
 # 显然，shu 这个 bit array 中值为 1 的那些位是不能放的。那么 pie 和 na 这两个 bit array 对当前行有什么影响呢？注意到第 row 行的各列对应的撇
@@ -182,6 +184,7 @@ class Solution3(object):
 # 中值为 1 的那些位就也不能放置皇后了。把三者或起来再取反：~(shu | (pie >> row) | (na >> (n - 1 - row)))，得到的结果中的最右 n 位就代表了
 # 能够放置皇后的位置。注意这个结果中，除了最右 n 位以外，左边的位中也会有一些 1，这些 1 是多余的，应当去掉。怎么去掉呢？可以用一个最右 n 位为 1、
 # 其它位为 0 的 bit array 与上述结果进行与运算，而这个 bit array 可以用 (1 << n) - 1 这个表达式制造出来。
+
 
 class Solution4(object):
     def solveNQueens(self, n):
@@ -211,7 +214,8 @@ class Solution4(object):
                 return
 
             # 当前可放皇后的列
-            available = ((1 << n) - 1) & ~(self.shu | (self.pie >> row) | (self.na >> (n - 1 - row)))
+            available = ((1 << n) - 1) & ~(self.shu |
+                                           (self.pie >> row) | (self.na >> (n - 1 - row)))
             # 将所有可放的列全部搜一遍
             while available:
                 # 从最后一个开始试
@@ -219,10 +223,14 @@ class Solution4(object):
                 available ^= p
 
                 col[row] = math.log(p, 2)
-                self.shu ^= p; self.pie ^= (p << row); self.na ^= (p << (n - 1 - row)) # 设置标记
+                self.shu ^= p
+                self.pie ^= (p << row)
+                self.na ^= (p << (n - 1 - row))  # 设置标记
                 solve(row + 1)
                 col[row] = -1
-                self.shu ^= p; self.pie ^= (p << row); self.na ^= (p << (n - 1 - row)) # 清除标记
+                self.shu ^= p
+                self.pie ^= (p << row)
+                self.na ^= (p << (n - 1 - row))  # 清除标记
 
         ts = time.time()
         solve(0)
@@ -230,6 +238,53 @@ class Solution4(object):
         print("total time: %f seconds" % (te - ts))
         return self.res
 
+# 利用形参
 
-s = Solution4()
+
+class Solution5(object):
+    def solveNQueens(self, n):
+        """
+        :type n: int
+        :rtype: List[List[str]]
+        """
+
+        col = [-1 for _ in range(n)]
+        self.res = []
+
+        def solve(row, shu, pie, na):
+
+            if row == n:
+                tmp = []
+                for i in range(n):
+                    s = ""
+                    for j in range(n):
+                        if j == col[i]:
+                            s += "Q"
+                        else:
+                            s += "."
+                    tmp.append(s)
+
+                self.res.append(tmp)
+                return
+
+            # 当前可放皇后的列
+            available = ((1 << n) - 1) & ~(shu | pie | na)
+            # 将所有可放的列全部搜一遍
+            while available:
+                # 从最后一个开始试
+                p = available & -available
+                available ^= p
+
+                col[row] = math.log(p, 2)
+                solve(row + 1, shu | p, (pie | p) >> 1, (na | p) << 1)  # 设置标记
+                col[row] = -1
+
+        ts = time.time()
+        solve(0, 0, 0, 0)
+        te = time.time()
+        print("total time: %f seconds" % (te - ts))
+        return self.res
+
+
+s = Solution5()
 print(s.solveNQueens(4))
