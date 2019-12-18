fc9760318d613748642e7fffbb3a4c05e84fc4ce
shan wei <swbuild@qq.com>
2019-12-18 22:43:11 +0800

    91

--- 0091. 解码方法/main.py
+++ 0091. 解码方法/main.py
@@ -12,13 +12,13 @@ class Solution(object):
             if len(tmps) == 0:
                 self.res += 1
                 return
-            
+
             for i in range(min(len(tmps), 2)):
                 if int(tmps[:i+1]) > 0 and int(tmps[:i+1]) <= 26:
                     solve(tmps[i+1:])
                 else:
                     return
-        
+
         solve(s)
         return self.res
 
@@ -31,26 +31,25 @@ class Solution(object):
         n = len(s)
         if n == 0:
             return 0
-        if s[0] == 0:
-            return 0
 
-        dp = [0 for _ in range(n)]
-        dp[0] = 1
+        dp = [0 for _ in range(n+1)]
+        dp[n] = 1
+        if (s[n-1] == '0'):
+            dp[n-1] = 0
+        else:
+            dp[n-1] = 1
 
-        for i in range(1,n):
+        for i in range(n-2, -1, -1):
             if int(s[i]) == 0:
-                if int(s[i-1:i+1])> 0 and int(s[i-1:i+1]) < 30:
-                    dp[i] = dp[i-1]
-                else:
-                    return 0
+                dp[i] = 0
             else:
-                dp[i] = dp[i-1]
-                if int(s[i-1:i+1])> 10 and int(s[i-1:i+1]) < 30:
-                    dp[i] += 1
-        
-        return dp[n-1]
+                if int(s[i:i+2]) <= 26:
+                    dp[i] = dp[i+1] + dp[i+2]
+                else:
+                    dp[i] = dp[i+1]
 
-            
+        return dp[0]
+
 
 s = Solution()
-print(s.numDecodings2("366122"))
+print(s.numDecodings2("110"))
