a4018931622cb29dea2ba6a202aad0a4873e73d3
shan wei <swbuild@qq.com>
2020-02-07 19:33:58 +0800

    122

---
+++ 0122. 买卖股票的最佳时机 II/main.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if len(prices) < 2:
+            return 0
+
+        res = 0
+        tmp = prices[0]
+        for i in range(1, len(prices)):
+            if prices[i] < prices[i-1]:
+                res += prices[i-1]-tmp
+                tmp = prices[i]
+            if i == len(prices)-1:
+                res += prices[i]-tmp
+
+        return res
+
+
+s = Solution()
+print(s.maxProfit([7,1,5,3,6,4]))
